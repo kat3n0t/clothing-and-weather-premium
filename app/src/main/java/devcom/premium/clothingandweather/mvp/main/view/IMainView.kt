a81fc4b4ecaf4bbb3c4831cedc1ca071241fd773
VladislaV <v.virtenberger@outlook.com>
2020-04-25 05:34:16 +0700

    refactor: добавлены комментарии к методам, переименован метод обновления

--- app/src/main/java/devcom/premium/clothingandweather/mvp/main/view/IMainView.kt
+++ app/src/main/java/devcom/premium/clothingandweather/mvp/main/view/IMainView.kt
@@ -9,24 +9,55 @@ import moxy.viewstate.strategy.StateStrategyType
 
 interface IMainView : MvpView {
 
+    /**
+     * Отображает стандартную модель персонажа
+     */
     @StateStrategyType(SkipStrategy::class)
     fun showDefaultModel()
 
+    /**
+     * Переключает видимость информации
+     *
+     * @param isCanVisible истина, если нужно отобразить инфу
+     */
     @StateStrategyType(SkipStrategy::class)
     fun switchInfoVisible(isCanVisible: Boolean)
 
+    /**
+     * Конвертирует погодные данные в текст и наполняет представление
+     *
+     * @param weather данные
+     */
     @StateStrategyType(SkipStrategy::class)
     fun setTextInfo(weather: Weather)
 
+    /**
+     * Запускает целевую активность
+     *
+     * @param targetClass класс целевой активности
+     */
     @StateStrategyType(SkipStrategy::class)
     fun launchActivity(targetClass: Class<*>)
 
+    /**
+     * Загружает иконку с погодой
+     *
+     * @param iconUri ссылка на иконку
+     */
     @StateStrategyType(SkipStrategy::class)
     fun loadIcon(iconUri: Uri)
 
+    /**
+     * Загружает модель по идентификатору
+     *
+     * @param id [DrawableRes]
+     */
     @StateStrategyType(SkipStrategy::class)
     fun loadModel(@DrawableRes id: Int)
 
+    /**
+     * Обновляет погодные данные
+     */
     @StateStrategyType(SkipStrategy::class)
-    fun updateAPIConnection()
+    fun updateWeatherData()
 }
