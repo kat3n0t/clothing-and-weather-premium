b7b8bf89f536a17279ec33ad0676ef49c807b1d8
VladislaV <v.virtenberger@outlook.com>
2020-12-20 04:35:33 +0700

    #17: преобразована Gender

--- app/src/main/java/devcom/premium/clothingandweather/common/Gender.kt
+++ app/src/main/java/devcom/premium/clothingandweather/common/Gender.kt
@@ -1,8 +1,9 @@
 package devcom.premium.clothingandweather.common
 
-annotation class Gender {
-    companion object {
-        const val MAN = 0
-        const val WOMAN = 1
-    }
+/**
+ * Пол модели персонажа
+ */
+enum class Gender {
+    MAN,
+    WOMAN
 }
--- app/src/main/java/devcom/premium/clothingandweather/common/Human.kt
+++ app/src/main/java/devcom/premium/clothingandweather/common/Human.kt
@@ -1,3 +1,3 @@
 package devcom.premium.clothingandweather.common
 
-data class Human(@Gender val gender: Int, @Style val style: Int)
+data class Human(val gender: Gender, @Style val style: Int)
