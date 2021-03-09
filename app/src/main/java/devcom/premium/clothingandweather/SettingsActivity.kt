f1ec291be47290b1b23df1578f27e9d7b485a129
VladislaV <v.virtenberger@gmail.com>
2021-03-10 03:13:07 +0700

    #48: fixed back buttons

--- app/src/main/java/devcom/premium/clothingandweather/SettingsActivity.kt
+++ app/src/main/java/devcom/premium/clothingandweather/SettingsActivity.kt
@@ -3,7 +3,6 @@ package devcom.premium.clothingandweather
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NavUtils
 
 /**
  * Активность для фрагмента настроек
@@ -20,7 +19,7 @@ class SettingsActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         android.R.id.home -> {
-            NavUtils.navigateUpFromSameTask(this)
+            onBackPressed()
             true
         }
         else -> super.onOptionsItemSelected(item)
